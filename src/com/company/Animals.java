ba63bb6018722900ce2c306732bcef9c38b23f92
mrLeks <58446622+mrLeks@users.noreply.github.com>
2019-12-25 15:51:46 +0300

    Update

--- src/com/company/Animals.java
+++ src/com/company/Animals.java
@@ -1,19 +1,63 @@
 package com.company;
 
 public class Animals {
-    public String name;
-    public int run;
+    protected String name;
 
-    public Animals() {
-    }
+    protected int runLimit = 0;
+    protected double jumpLimit = 0;
+    protected int swimLimit = 0;
+
+    protected int appetite;
+    protected boolean hungry;
+
+    public Animals() { this.name = "Безымянный"; }
 
     public Animals(String name) {
         this.name = name;
     }
 
+    public void run(int distance){
+        if(runLimit > 0 && distance <= runLimit){
+            System.out.println(this.name + " пробежал " + distance + " метров");
+
+        }
+        else {
+            System.out.println(this.name + " не сможет пробежать столько!");
+        }
+    }
+
+    public void swim(int distance){
+        if(swimLimit > 0 && distance <= swimLimit){
+            System.out.println(this.name + " проплыл " + distance + " метров");
+
+        }
+        else {
+            System.out.println(this.name + " не сможет проплыть столько!");
+        }
+    }
+
+    public void jump(double height){
+        if(jumpLimit > 0 && height <= jumpLimit){
+            System.out.println(this.name + " пробежал " + height + " метров");
+
+        }
+        else {
+            System.out.println(this.name + " не сможет пробежать столько!");
+        }
+    }
+
     public void animalInfo() {
         System.out.println();
+    }
 
+    void info() {
+        String isHungry = !hungry ? "после тренировки плотно покушал и доволен" : "после тренировки проголодался, но в тарелке нет еды!";
+        System.out.println(name + ": " + isHungry);
+    }
+
+    void eat(Plate plate) {
+        if (hungry && plate.reduce(appetite))
+            hungry = false;
     }
     public static void Main(String[] args){
 
